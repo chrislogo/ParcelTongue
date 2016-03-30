ee9015c653b224bb465d7361330615a007a1308b
chrislogo <chris.loguidice92@gmail.com>
2016-03-29 22:09:00 -0400

    Update ChatMessage.java

--- app/src/main/java/group/project2/ChatMessage.java
+++ app/src/main/java/group/project2/ChatMessage.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package group.project2;
 
 public class ChatMessage {
@@ -11,17 +11,3 @@ public class ChatMessage {
         this.message = message;
     }
 }
-=======
-package group.project2;
-
-public class ChatMessage {
-    public boolean left;
-    public String message;
-
-    public ChatMessage(boolean left, String message) {
-        super();
-        this.left = left;
-        this.message = message;
-    }
-}
->>>>>>> c9d8335f306ffb79dd06260acf24cc72a5f14f97
